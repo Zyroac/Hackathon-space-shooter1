e9ac4982552fa6dbe42a76b5132ac6448e1c99b8
Uschi361 <dranoel-u@wp.pl>
2019-09-21 17:07:06 +0200

    LeoU imported Game Logic

---
+++ Assets/Workspace_LeoU/MyScripts/BulletMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMover : MonoBehaviour
+{
+    public Transform super;
+    public float speed=15;
+    public float lifeTime=10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {       
+        var body = GetComponent<Rigidbody>();
+        var superBody=super.GetComponent<Rigidbody>();
+        body.velocity=transform.forward*speed + superBody.velocity;
+        
+        Destroy(gameObject, lifeTime);
+    }
+
+    void Update()
+    {
+    }
+
+}
