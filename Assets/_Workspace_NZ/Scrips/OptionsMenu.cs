f8915bc13a772eff5952fcd8323f245368b8700b
niklas z <niki10@gmx.de>
2019-09-21 10:57:09 +0200

    initial commit

---
+++ Assets/_Workspace_NZ/Scrips/OptionsMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+     void Start()
+    {
+       resolutions = Screen.resolutions;
+        resolutionDropdown.ClearOptions();
+
+        List<string> Options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + "x" + resolutions[i].height;
+            Options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(Options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+    }
+
+    public void SetResolution (int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume (float Lautstärke)
+    {
+        audioMixer.SetFloat("Lautstärke",Lautstärke);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
---
+++ Assets/_Workspace_NZ/Scrips/PausenMenü.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PausenMenü : MonoBehaviour
+{
+    public static bool SpielIstPausiert = false;
+    public GameObject PausenMenüUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (SpielIstPausiert)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume ()
+    {
+        PausenMenüUI.SetActive(false);
+        Time.timeScale = 1f;
+        SpielIstPausiert = false;
+    }
+
+    void Pause ()
+    {
+        PausenMenüUI.SetActive(true);
+        Time.timeScale = 0f;
+        SpielIstPausiert = true;
+    }
+
+    public void LadeMenü()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void SpielBeenden()
+    {
+        Application.Quit();
+    }
+}
